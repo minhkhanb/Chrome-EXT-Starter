44183e6edcc315647be6ea76345721046d59d63b
KhaLe <kha.le@mtech.vn>
2017-04-21 11:12:53 +0700

    init

---
+++ start.js
@@ -0,0 +1,6 @@
+console.log('Hello world');
+var h1 = document.createElement("h1");
+h1.innerHTML = "năm nay 30/4 có plan gì ko chú Việt - Tuấn ?";
+h1.style.fontSize = '72px';
+document.body.textContent = '';
+document.body.appendChild(h1);
